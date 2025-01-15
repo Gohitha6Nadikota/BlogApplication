338aaf124952aa214460a27c1fda2195822e034f
GohithaNadikota <gohithapriyanadikota@gmail.com>
2025-01-15 22:00:46 +0530

    Profile Edit

--- BlogApplication/forms.py
+++ BlogApplication/forms.py
@@ -9,22 +9,24 @@ for x in catags:
 class Form(forms.ModelForm):
     class Meta:
         model=Post
-        fields=('title', 'title_tag','author','category','body')
+        fields=('title', 'title_tag','author','category','body','snippet')
         widgets={
             'title':forms.TextInput(attrs={'class':'form-control mt-2','placeholder':'Enter your title'}),
             'title_tag':forms.TextInput(attrs={'class':'form-control mt-2','placeholder':'Enter your title tag'}),
             'author':forms.TextInput(attrs={'class':'form-control mt-2','id':'Authorname','value':'','disabled':'disabled'}),
             'category':forms.Select(choices=catags_list,attrs={'class':'form-control mt-2'}),
             'body':forms.Textarea(attrs={'class':'form-control mt-2','placeholder':'Enter your content'}),
+            'snippet':forms.Textarea(attrs={'class':'form-control mt-2'})
         }
 
 
 class EditForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields=('title', 'title_tag','body')
+        fields=('title', 'title_tag','body','snippet')
         widgets={
             'title':forms.TextInput(attrs={'class':'form-control mt-2','placeholder':'Enter your title'}),
             'title_tag':forms.TextInput(attrs={'class':'form-control mt-2','placeholder':'Enter your title tag'}),
             'body':forms.Textarea(attrs={'class':'form-control mt-2','placeholder':'Enter your content'}),
+            'snippet':forms.Textarea(attrs={'class':'form-control mt-2','placeholder':'Enter the snippet content'})
         }
--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView,UserEditView
 urlpatterns = [
    path('register/',UserRegisterView.as_view(),name="register"),
+   path('editinfo/',UserEditView.as_view(),name="editinfo")
 ]
